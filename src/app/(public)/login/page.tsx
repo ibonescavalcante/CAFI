81224c58fb7965f7ee35be2eead9da5c131469a7
isasolucoes <isasolucoes@gmail.com>
2024-12-21 12:30:54 -0300

    feat:[create actions.ts for login]

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,34 +1,58 @@
+import { useActionState } from "react";
+import { login } from "./actions";
+
 export default function Page() {
+  const [state, loginAction] = useActionState(login, undefined);
   return (
     <>
-    <div className="flex items-center justify-center min-h-screen bg-white ">
+      <div className="flex items-center justify-center min-h-screen bg-white ">
+        <div className=" form-container w-full max-w-sm">
+          {/* <div className="w-full p-4 flex justify-center"><img width="100" src="img/pmp.png"/></div> */}
+          <h1
+            style={{
+              textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            }}
+            className="text-3xl font-bold te text-gray-800 text-center mb-4 "
+          >
+            CMEP
+          </h1>
 
-      <div className=" form-container w-full max-w-sm">
-      {/* <div className="w-full p-4 flex justify-center"><img width="100" src="img/pmp.png"/></div> */}
-       <h1 style={{
-        textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'
-       }} className="text-3xl font-bold te text-gray-800 text-center mb-4 ">CAFI</h1>
-       
-        <form>
-          <div className="mb-4">
-            {/* <label htmlFor="email" className="block  text-gray-600 mb-1">Usuário</label> */}
-            <input type="text" required name="usuario" placeholder="Usuário" className="w-full font-light p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"/>
+          <form action={loginAction}>
+            <div className="mb-4">
+              {/* <label htmlFor="email" className="block  text-gray-600 mb-1">Usuário</label> */}
+              <input
+                type="text"
+                required
+                name="usuario"
+                placeholder="Usuário"
+                className="w-full font-light p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+              />
+            </div>
+
+            <div className="mb-6">
+              {/* <label htmlFor="password" className="block text-gray-600 mb-1">Senha</label> */}
+              <input
+                type="password"
+                required
+                id="password"
+                name="password"
+                placeholder="Senha"
+                className="w-full p-2  font-light rounded-md border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full py-3 bg-[#376D2B] shadow-lg hover:bg-[#385d2b] text-white font-semibold rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            >
+              Login
+            </button>
+          </form>
+          <div className="p-8 flex justify-center">
+            <span className="text-sm font-light">Lot of love DTIC@2024</span>
           </div>
-
-          <div className="mb-6">
-            {/* <label htmlFor="password" className="block text-gray-600 mb-1">Senha</label> */}
-            <input type="password"required  id="password" name="password"placeholder="Senha"  className="w-full p-2  font-light rounded-md border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"/>
-          </div>
-
-          <button type="submit" className="w-full py-3 bg-[#376D2B] shadow-lg hover:bg-[#385d2b] text-white font-semibold rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">Login</button>
-        </form>
-        <div className="p-8 flex justify-center">
-        <span className="text-sm font-light">Lot of love DTIC@2024</span>
         </div>
-      </div>   
-    
-    </div>
-   
+      </div>
     </>
-  )
+  );
 }
